667417dff419490ddbd5c7c68975dbfaa287c872
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-28 20:14:15 +0600

    add: Seller info verification contract

--- contracts/SellerVerif.sol
+++ contracts/SellerVerif.sol
@@ -8,33 +8,66 @@ contract SellerVerif {
     enum VerfificationStatus {
         NOT_PROCESSED,
         VERIFIED,
-        UNVERIFIED
+        UNVERIFIED,
+        INVALID
     }
 
-    struct data {
+    struct Data {
         string nid;
-        string name;
+        bytes32 name;
+        bytes32 sellerAddress;
     }
 
-    mapping (address => VerfificationStatus) private sellerVerification;
-    mapping (address => data) private verificationData;
+    mapping (bytes32 => VerfificationStatus) private sellerVerification;
+    mapping (address => uint) private sellerIndex;
+    Data[] private sellerData;
 
-    // constructor (address _verifierContract) {
-    //     verifierContract = IVerification(_verifierContract);
-    // }
+    constructor (address _verifierContract) {
+        verifierContract = IVerification(_verifierContract);
+        sellerData.push(Data({
+            nid: "",
+            name: "",
+            sellerAddress: 0
+        }));
+    }
 
     modifier onlyVerifier (address _user) {
         require(verifierContract.confirmVerifier(_user), "Only verifier allowed");
         _;
     }
+
+    modifier onlyNonVerified (uint index, address _user) {
+        bytes32 addressHash = keccak256(abi.encodePacked(_user));
+        require(sellerVerification[sellerData[index].sellerAddress] != VerfificationStatus.VERIFIED, "Verified merchants not allowed");
+        require(sellerData[index].sellerAddress != addressHash, "Can't verify own");
+        _;
+    }
+
+    function submitData(string memory _nid, bytes32 _name) external{
+        if (sellerIndex[msg.sender] == 0) {
+            sellerData.push(Data({
+                nid: _nid,
+                name: _name,
+                sellerAddress: keccak256(abi.encodePacked(msg.sender))
+            }));
+            sellerIndex[msg.sender] = sellerData.length - 1;
+        } else {
+            sellerData[sellerIndex[msg.sender]].nid = _nid;
+            sellerData[sellerIndex[msg.sender]].name = _name;
+        }
+    }
     
 
     function viewStatus(address _user) external view returns(VerfificationStatus){
-        return sellerVerification[_user];
+        return sellerVerification[keccak256(abi.encodePacked(_user))];
     }
 
-    function verifySeller(address _user) external {
-        sellerVerification[_user] = VerfificationStatus.VERIFIED;
+    function verifySeller(uint id, VerfificationStatus status, string memory _name) onlyVerifier(msg.sender) onlyNonVerified(id, msg.sender) external {
+        if (status == VerfificationStatus.VERIFIED && keccak256(abi.encodePacked(_name)) == sellerData[id].name) {
+            sellerVerification[sellerData[id].sellerAddress] = VerfificationStatus.VERIFIED;
+        } else if (status == VerfificationStatus.UNVERIFIED) {
+            sellerVerification[sellerData[id].sellerAddress] = VerfificationStatus.UNVERIFIED;
+        }
     }
 
 }
