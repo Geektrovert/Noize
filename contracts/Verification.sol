692730dc779a8fe955dbb0e775b775b0fe984ab8
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-28 11:40:40 +0600

    add: Verifier constructor

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -3,4 +3,22 @@ pragma solidity >= 0.8.4;
 
 contract Verifier {
     mapping (address => bool) private isVerifier;
+    mapping (address => uint) private deposit;
+    address private securityToken;
+
+    constructor (address _tokenAddress) {
+        securityToken = _tokenAddress;
+    }
+
+    modifier notVerifier(address _user) {
+        require(!isVerifier[_user], "User is already a verifier");
+        _;
+    }
+
+    function signUp() notVerifier(msg.sender) external {
+        // TODO: add security deposit
+        isVerifier[msg.sender] = true;
+    }
+
+
 }
