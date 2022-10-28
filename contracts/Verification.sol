2eec6432ba82db6d617b5343a2987573cba467cd
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-28 23:03:14 +0600

    remove: ERC-20 token from verifier contract

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -4,12 +4,6 @@ import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 
 contract Verifier {
     mapping (address => bool) private isVerifier;
-    mapping (address => uint) private deposit;
-    address private securityToken;
-
-    constructor (address _tokenAddress) {
-        securityToken = _tokenAddress;
-    }
 
     modifier notVerifier(address _user) {
         require(!isVerifier[_user], "User is already a verifier");
