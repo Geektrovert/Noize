90048d0bbd290814ded6e6e43c8712b27c296a09
Jason Haque <zahinjason220434@gmail.com>
2022-10-28 12:40:08 +0600

    Add(function) product create

--- contracts/store.sol
+++ contracts/store.sol
@@ -4,11 +4,43 @@ import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 
 contract Store{
     struct Product{
-        string id,
-        string name,
-        address buyer,
-        address creator,
-        uint index,
-        uint price
+        uint id;
+        string name;
+        address owner;
+        address creator;
+        uint price;
+        string image_url;
+        bool sellable;
     }
+    Product[] private products;
+    address private tokenAddress;
+    address private contractOwner;
+
+    function createProduct(
+        string memory _name,
+        uint _price,
+        string memory _image_url
+    ) external{
+        Product memory newProduct = Product({
+            id: products.length,
+            name: _name,
+            owner: msg.sender,
+            creator: msg.sender,
+            price: _price,
+            image_url: _image_url,
+            sellable: true
+        });
+        products.push(newProduct);
+    }
+
+    function getAllProducts(){
+
+    }
+
+    function buyproduct(){
+
+    }
+
+
+
 }
