ab569fcb1fabead657fbcf3ea8d371248e20e56e
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-28 11:54:05 +0600

    add: Security deposit code

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.4;
+import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 
 contract Verifier {
     mapping (address => bool) private isVerifier;
@@ -16,9 +17,8 @@ contract Verifier {
     }
 
     function signUp() notVerifier(msg.sender) external {
-        // TODO: add security deposit
+        IERC20(securityToken).transferFrom(msg.sender, address(this), 10*10**18);
         isVerifier[msg.sender] = true;
     }
-
 
 }
