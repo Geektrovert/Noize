73997fa5d6f768baa0d0f386c5beacef105e404c
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-29 08:19:05 +0600

    update: Hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    alfajores: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+  },
+};
