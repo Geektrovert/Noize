fe4ea63551c3bddc11a9797bb1f61e9c0d361d6a
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-29 08:01:56 +0600

    add: Hardhat deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import _ from "@nomiclabs/hardhat-waffle";
 dotenv.config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
