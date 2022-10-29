f047d47e31e9b1c57ebb0c30b84fd6fd22d215dc
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-29 10:48:50 +0600

    Update(config): constants

--- src/configs/constants/index.ts
+++ src/configs/constants/index.ts
@@ -2,3 +2,5 @@ export const STORE_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_STORE_CONTRACT;
 export const DIGEST = process.env.DIGEST;
 export const SECRET_KEY = process.env.SECRET_KEY;
 export const WEB3_STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN;
+export const USDC_ADDRESS = process.env.NEXT_PUBLIC_USDC_CONTRACT;
+export const USDC_MULTIPLIER = 1e6;
