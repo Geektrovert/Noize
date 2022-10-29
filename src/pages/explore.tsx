4239efe3bd86c24dac26f4ccb317ff445737d833
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-29 10:11:07 +0600

    Add(UI): implement Explore page

---
+++ src/pages/explore.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+import Products from "../components/products";
+
+const Index = () => (
+  <Box h="100%">
+    <Products />
+  </Box>
+);
+
+export default Index;
