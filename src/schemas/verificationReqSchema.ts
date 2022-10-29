4af47dfe38c2d58438e349f1eacb13917a5537bb
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-29 14:22:13 +0600

    Fix(integration): verification encryption

--- src/schemas/verificationReqSchema.ts
+++ src/schemas/verificationReqSchema.ts
@@ -2,9 +2,9 @@ import { object, string, number, InferType } from "yup";
 
 export const verificationSchema = object({
   name: string().required("Name is required"),
-  nid: number().required("NID is required").typeError("NID must be a number").
+  nid: string().required("NID is required").typeError("NID must be a number").
     test("len", "NID must be 10 digits", (value) => {
-      return value?.toString().length === 10;
+      return value?.length === 10;
     }),
 });
 
