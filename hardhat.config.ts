1b0242003ee86c4d489d94260a6b59ec4e0215a6
Sihan Tawsik <sihantawsik@gmail.com>
2022-10-29 07:46:05 +0600

    add: Hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+  },
+};
